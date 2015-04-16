c0bfd79fb3bd9824ca1c9963c02f1dd4bd4651b3
marbiru <ubram@Uris-MacBook-Air.local>
2015-04-16 14:53:23 +0200

    Separating out more functions
    now everything is separate, has its own function

--- core.js
+++ core.js
@@ -44,9 +44,9 @@ var translation_list = [
 "Don't let anyone ever make you feel like you don't deserve what you want."
 ];
 
-// This initialises the easter-egg counter. Every X translations the translator does something "special", like translating into Beeps instead of into English
+// This initialises the translation counter. Every X translations the translator does something "special", like translating into Beeps instead of into English
 
-var easter_eggs = 0
+var turn_count = 0
 
 // This makes the 'Translate' button print an English-language translation of the 'input' field
 
@@ -54,7 +54,7 @@ document.getElementById("translate_button").onclick = print_translation;
 
  //if the input happens to be zero-length then the easter eggs (which manipulate the input text in various ways) wouldn't actually work. Perhaps this can be improved somehow later.
 function zero_meeps(){
-  document.getElementById("translation").innerHTML = "(a MEEPLESS silence)";
+  document.getElementById("translation").innerHTML = "(a meepless silence)";
 }
 
 //this easter egg just gives you back what you put in but with Meeps turned into Beeps and Mees into Bees
@@ -63,26 +63,52 @@ function first_easter_egg(){
 }
 
 function second_easter_egg(){
-   //this easter egg just gives back what you put in but with the Mees and Meeps reversed. So e.g. "Meep Mee Mee Meep" would turn into "Mee Meep Meep Mee". Amusingly, the code needs a "placeholder" thingumajig because if you just tell it to replace Meep --> Mee and Mee --> Meep then EVERYTHING ends up as Meep... because all the Meeps get changed to Mees, first, and then everything gets turned back into Meeps! Perhaps there is a better way to write this code so that all substitutions happen simultaneously but I don't know how to do that.
+//this easter egg just gives back what you put in but with the Mees and Meeps reversed. So e.g. "Meep Mee Mee Meep" would turn into "Mee Meep Meep Mee". Amusingly, the code needs a "placeholder" thingumajig because if you just tell it to replace Meep --> Mee and Mee --> Meep then EVERYTHING ends up as Meep... because all the Meeps get changed to Mees, first, and then everything gets turned back into Meeps! Perhaps there is a better way to write this code so that all substitutions happen simultaneously but I don't know how to do that.
       document.getElementById("translation").innerHTML = document.getElementById("input").innerHTML.replace(/Meep/g, 'Placeholder').replace(/Mee/g, 'Meep').replace(/Placeholder/g, 'Mee');
 }
 
+// this function runs any applicable easter eggs
+function run_easter_eggs(){
+  if (turn_count === 3) {
+    first_easter_egg();
+  } else if (turn_count === 7) {
+    second_easter_egg();
+  } else {
+    return;
+  }  
+}
+
+// these functions handle the "normal" translation cases
+
+function right_length(){
+  var meep_count = document.getElementById("input").innerHTML.split(" ").length - 1;
+  document.getElementById("translation").innerHTML = translation_list[meep_count];
+}
+
+function too_long(){
+  // if input string is too long then we just return an error message basically saying "woah, your string is too long". Hopefully the user will then "clear meeps" before continuing
+  document.getElementById("translation").innerHTML = "Woah woah woah, that's a bit meeping much.";  
+}
+
+function normal_translation(){
+  var meep_count = document.getElementById("input").innerHTML.split(" ").length - 1;
+  if (meep_count < translation_list.length) {
+     right_length();
+  } else {
+     too_long();
+    }
+}
 
 function print_translation() {
-  easter_eggs = easter_eggs + 1;
   var meep_count = document.getElementById("input").innerHTML.split(" ").length - 1;
+  turn_count = turn_count + 1;
   //increments the easter_egg counter. Every time someone clicks "translate" the easter_egg counter goes up by one, so that we can make it happen that every X translations the translator does something cool/interesting/different from the norm.
   if (meep_count === 0) {
     zero_meeps();
-  } else if (easter_eggs == 3) {
-    first_easter_egg();  
-  } else if (easter_eggs == 7) {
-    second_easter_egg();
+  } else if (turn_count === 3 || turn_count === 7) {
+    run_easter_eggs();
   // check that input string is not longer than the longest English sentence in our dictionary. If the input string is short enough that we have a suitable translation, just do that translation.
-  } else if (meep_count < translation_list.length) {
-     document.getElementById("translation").innerHTML = "&nbsp;" + translation_list[meep_count];
-  } else {
-  // if input string is too long then we just return an error message basically saying "woah, your string is too long". Hopefully the user will then "clear meeps" before continuing
-     document.getElementById("translation").innerHTML = "&nbsp;" + "Woah woah woah, that's a bit meeping much.";   
+  } else  {
+   normal_translation();
       }
   }
